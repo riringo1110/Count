878c92b2d725aa5c16613ec608991476e19bf3dd
Ririko Nagaishi <riry.cucumber@gmail.com>
2021-02-03 23:44:54 +0900

    カウントアプリ

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+   
+        if number >= 10 {
+            label.textColor = UIColor.red
+        }else{
+            label.textColor = UIColor.black
+        }
+    
+    
+    }
 
+    @IBAction func minus() {
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    @IBAction func double() {
+        number = number * 2
+        label.text = String(number)
+    }
+    
+    @IBAction func devide() {
+        number = number / 2
+        label.text = String(number)
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(0)
+    }
 }
 
