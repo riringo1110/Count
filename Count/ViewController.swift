53c6098e308bb3c9087267dad46cb641bbdecaff
Ririko Nagaishi <riry.cucumber@gmail.com>
2021-02-03 19:53:24 +0900

    Initial Commit

---
+++ Count/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by Ririko Nagaishi on 2021/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
